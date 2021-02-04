45f09cbcf6978febc5f24f98acb9fc1d90fc0d1b
Mayasa79 <mayasa.sabie@gmail.com>
2021-02-04 12:10:56 -0600

    deleted some lines

--- src/test/java/com/trycloud/tests/US003_TC007_Mayasa.java
+++ src/test/java/com/trycloud/tests/US003_TC007_Mayasa.java
@@ -11,16 +11,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class US003_TC007_Mayasa extends TestBase {
-    MenuBarPOM driverMenuBar = new MenuBarPOM();
 
-    @BeforeMethod
-    public void SetUpElements() {
 
-        PageFactory.initElements(Driver.getDriver(), driverMenuBar);
-    }
-    @Test
-    //Test case #7 - verify users can upload a file inside a folder
-    public void US3_TC7() throws InterruptedException {
         //Test case #7 - verify users can upload a file inside a folder
         // 1.Login as a user
         // 2.Choose a folder from the page
@@ -28,17 +20,10 @@ public class US003_TC007_Mayasa extends TestBase {
         // 4.Click “upload file”
         // 5.Upload a file
         // 6.Verify the file is displayed on the page
-        LogInPagePOM logInPagePOM=new LogInPagePOM();
-        logInPagePOM.inputUserName.click();
-        logInPagePOM.inputPassword.click();
-        logInPagePOM.buttonLogin.click();
 
-      WebElement fileButton=Driver.getDriver().findElement(By.className("active"));
-      fileButton.click();
     }
 
 //
 
 
 
-}
