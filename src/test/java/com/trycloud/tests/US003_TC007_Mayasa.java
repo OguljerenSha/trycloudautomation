e8e080385e7db6fc281583bf59213c53c53a87f3
Mayasa79 <mayasa.sabie@gmail.com>
2021-02-04 00:13:30 -0600

    Test1

---
+++ src/test/java/com/trycloud/tests/US003_TC007_Mayasa.java
@@ -0,0 +1,15 @@
+package com.trycloud.tests;
+
+import com.trycloud.Pages.LogInPagePOM;
+import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class US003_TC007_Mayasa extends TestBase {
+
+
+
+
+
+}
